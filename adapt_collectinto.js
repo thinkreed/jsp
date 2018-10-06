b1c8c1066fd186d436cc34f43cefe0c5e8033ef1
thinkreed <thinkreed2017@outlook.com>
2018-10-06 20:00:12 +0800

    adapt collectinto

---
+++ adapt_collectinto.js
@@ -0,0 +1,6 @@
+const collectInto = fn => (...args) => fn(args);
+const Pall = collectInto(Promise.all.bind(Promise));
+let p1 = Promise.resolve(1);
+let p2 = Promise.resolve(2);
+let p3 = new Promise(resolve => setTimeout(resolve, 2000, 3));
+Pall(p1, p2, p3).then(console.log);
